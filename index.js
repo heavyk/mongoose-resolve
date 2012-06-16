ca1c5f97f51e2ab6b1fc15bb7de76314340dc0ee
flames of love <mechanicofthesequence@gmail.com>
2012-06-16 07:03:27 +0200

    export the resolve function

--- index.js
+++ index.js
@@ -8,50 +8,20 @@
  * Version.
  */
 
-exports.version = '0.0.4';
+exports.version = '0.0.1';
 
 /**
  * Module dependencies.
  */
 
-var res = require('http').ServerResponse.prototype
-  , Promise = require('mongoose').Promise
-  , Query = require('mongoose').Query
-
-/**
- * Wrap the original rendering methods with support
- * for Queries and Promises.
- */
-
-res.render = wrap(res.render);
-res.partial = wrap(res.partial);
-
-function wrap (method) {
-  return function expressmongoose (view, options, callback, parent, sub) {
-
-    if (!options || 'function' == typeof options) {
-      return method.call(this, view, options, callback, parent, sub);
-    }
-
-    var self = this;
-    return resolve(options, function (err, result) {
-      if (err) {
-        return 'function' == typeof callback
-          ? callback(err)
-          : self.req.next(err);
-      }
-
-      // must return here so partials always work
-      return method.call(self, view, result, callback, parent, sub);
-    });
-  }
-}
+var Promise = require('mongoose').Promise
+  , Query = require('mongoose').Query;
 
 /**
  * Resolves any Queries and Promises within the passed options.
  */
 
-function resolve (options, callback, nested) {
+exports.resolve = function resolve (options, callback, nested) {
   var keys = Object.keys(options)
     , i = keys.length
     , remaining = []
@@ -109,69 +79,31 @@ function resolve (options, callback, nested) {
 }
 
 /**
- * Add Promise/Query support to res.send.
+ * Wrap a passed method to resolves all Promise/Query(s)
+ * before calling itself.
  */
 
-var send = res.send;
-res.send = function (body, headers, status) {
-  var self = this;
+exports.wrap = function wrap (method) {
+	return function (body, headers, status) {
+		var self = this;
 
-  function handleResult (err, result) {
-    if (err) return self.req.next(err);
-    send.call(self, result, headers, status);
-  }
+	  function handleResult (err, result) {
+	    if (err) return self.req.next(err);
+	    send.call(self, result, headers, status);
+	  }
 
-  if (body instanceof Promise) {
-    return body.addBack(handleResult);
-  }
+	  if (body instanceof Promise) {
+	    return body.addBack(handleResult);
+	  }
 
-  if (body instanceof Query) {
-    return body.run(handleResult);
-  }
+	  if (body instanceof Query) {
+	    return body.run(handleResult);
+	  }
 
-  if ('Object' == body.constructor.name) {
-    return resolve(body, handleResult);
-  }
+	  if ('Object' == body.constructor.name) {
+	    return resolve(body, handleResult);
+	  }
 
-  send.call(this, body, headers, status);
+	  method.call(this, body, headers, status);
+	};
 };
-
-/**
- * Extends res.redirect with mongoose Promise support.
- *
- * Does not accept Queries since those return documents.
- * Instead, manually handle the result of your query first,
- * then resolve your promise with the url and optional status.
- *
- *     var promise = new mongoose.Promise;
- *     res.redirect(promise);
- *
- *     // later...
- *     promise.complete(url [, status]);
- *
- * The promise can pass an optional status code as the
- * second argument.
- *
- *     promise.complete('/elsewhere', 301);
- */
-
-var redirect = res.redirect;
-res.redirect = function (url, status) {
-  var self = this;
-
-  function handleResult (err, result, code) {
-    if (err) return self.req.next(err);
-
-    if ('string' != typeof result) {
-      return self.req.next(new Error('URL Expected'));
-    }
-
-    redirect.call(self, result, code || status);
-  }
-
-  if (url instanceof Promise) {
-    return url.addBack(handleResult);
-  }
-
-  redirect.call(this, url, status);
-}
